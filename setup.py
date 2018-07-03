5c472addf5119acafd3cab3e09f53da6eb9cca6a
gurgalex <gurgalex@iastate.edu>
2018-07-02 21:21:28 -0500

    Bump version to 0.5

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='bot_api',
-      version='0.4',
+      version='0.5',
       py_modules=['bot_api'],
       url="https://github.com/cml-cs-iastate/bot_api",
       author="iastate",
